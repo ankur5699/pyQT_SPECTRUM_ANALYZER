7d741b98dde56d6462476707f88e0cd17d27ef7a
ankur <ankur5699@gmail.com>
2024-04-30 21:21:46 +0530

    rust stuff

--- test_code_rust/src/main.rs
+++ test_code_rust/src/main.rs
@@ -1,5 +1,5 @@
 use std::f64::consts::PI;
-// use std::net::UdpSocket;
+use std::net::UdpSocket;
 use std::io;
 // use rand::Rng;
 
@@ -19,7 +19,10 @@ fn main() -> io::Result<()> {
     //We convert them to 16 bit hex values, and then send them as bytes on 127.0.0.1:5006
 
     let quantized_buffer = convert2hex_16bit(signal_buffer);
-    println!("{:?}",quantized_buffer);
+    println!("{:04X?}",quantized_buffer[5]);
+    let packet_buffer = packetize(quantized_buffer);
+    println!("{:02X?}",packet_buffer[10]);
+    println!("{:02X?}",packet_buffer[11]);
     
     //We have Converted the signal to 16 bit values
     //To send this as a packet, we need to send this as bytes (u8), So we split the packets
@@ -29,21 +32,18 @@ fn main() -> io::Result<()> {
 
     // let mut rng = rand::thread_rng();
     // Connect to the server (replace with the server's IP address and port)
-    // let server_address = "127.0.0.1:5006";
-    // let socket = UdpSocket::bind("0.0.0.0:0")?;
-    // socket.connect(server_address)?;
+    let server_address = "127.0.0.1:5006";
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+    socket.connect(server_address)?;
     // let buffer: Vec<u32> = (0..1024).collect();
 
-    /*
     loop{
-        let noise: Vec<u8> = (0..16384).map(|_| rng.gen()).collect();
-        println!("{:?}",buffer);
-        let slice_data: &[u32] = buffer.as_slice();
+        // let noise: Vec<u8> = (0..16384).map(|_| rng.gen()).collect();
+        // println!("{:?}",buffer);
+        // let slice_data: &[u32] = buffer.as_slice();
         // Send the message to the server
-        //socket.send(slice_data)?;
-
+        socket.send(&packet_buffer)?;
     } 
-    */
     Ok(())
 }
 
@@ -78,6 +78,38 @@ fn convert2hex_16bit(buffer:[f64; NUM_SAMPLES]) -> [i16; NUM_SAMPLES]{
 
     } 
     return quantized_buffer;
+}
+
+//0xdeadbeef should become 0xdead and 0xbeef
+fn packetize(buffer:[i16; NUM_SAMPLES]) -> [u8; 2*NUM_SAMPLES]{
+    let mut packet_buffer = [0 as u8; 2*NUM_SAMPLES];
+    let packet_size = NUM_SAMPLES * 2;
+
+    /*
+    for i in 1..packet_size{
+        if i%2 != 0 {
+            continue;
+        }
+        packet_buffer[i/2] = (buffer[i/2] >> 8) as u8;
+        packet_buffer[(i/2)+1] = (buffer[i/2]) as u8;
+//        print!("{:04X?}\t", buffer[i/2]);
+//        print!("{:02X?}\t", packet_buffer[i/2]);
+//        print!("{:02X?}\n", packet_buffer[(i/2) + 1]);
+        println!("{}",(i/2));
+        println!("{}",((i/2)+1));
+    }
+ */
+    let mut i:usize = 0;
+    loop{
+        if i==packet_size {
+            break;
+        }
+        packet_buffer[i] = (buffer[i/2] >> 8) as u8;
+        packet_buffer[i+1] = (buffer[i/2]) as u8;
+        // println!("{}",i);
+        i = i + 2;
+    }
+    return packet_buffer;
 }
 
 
